0f12ca90f75441eb8b527b8f7ea246173a589720
priyankakri <84251144+priyankakri@users.noreply.github.com>
2024-02-05 19:40:48 +0530

    Update main.tf
    added backend block

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = "~> 1.5.6"
 }
+terraform {
+  backend "s3" {
+    bucket = "test-bucket-1212012"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
 provider "aws" {
   region = "us-east-1"
